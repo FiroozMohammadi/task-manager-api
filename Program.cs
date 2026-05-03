d5c6d385aea050a1421ebecf44da35d3673b27ad
Firooz Ahmad Mohammadi <105348089+FiroozMohammadi@users.noreply.github.com>
2026-05-03 11:30:01 +0300

    Fix PostgreSQL connection

--- Program.cs
+++ Program.cs
@@ -5,21 +5,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
-// ✅ FIXED CORS (ONLY ONCE)
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReact",
         policy =>
         {
-            policy
-                .AllowAnyOrigin()
-                .AllowAnyHeader()
-                .AllowAnyMethod();
+            policy.WithOrigins("https://tourmaline-pithivier-6275a6.netlify.app")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
         });
 });
 
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
